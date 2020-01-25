1d68b68423065fb81c545fb06a273528fd0ae3e8
stephan <stephan_000@hotmail.com>
2020-01-25 19:30:30 +0100

    Projectile Scripts
    Added a Prefab for a projectile
    Created a crosshair that is a substitude for the mouse cursor
    Added the necessary Scripts to have a Projectile, a Spawner for projectiles and the "Shoot" action from the characted

---
+++ Assets/Scripts/FollowMouseCursor.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowMouseCursor : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject m_Crosshair = null;
+    void Start()
+    {
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        m_Crosshair.transform.position = Input.mousePosition;
+    }
+}
