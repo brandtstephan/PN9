3a5b0ea49f39821cc6f43812d5426e0729956922
Andre <ajq0@protonmail.com>
2020-01-26 19:17:08 +0100

    working on jump, made a new scene called movement

---
+++ Assets/Scripts/Jump.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jump : MonoBehaviour
+{
+    public float strength = 5f;
+    public int maxJumpCount = 2;
+    public float fallMultiplier = 2f;
+    public float jumpTime = 1.0f;
+    Rigidbody2D rb;
+    public int jumpCount = 0;
+    bool canJump = true;
+    float distToGround;
+
+    // https://www.gamasutra.com/blogs/DanielFineberg/20150825/244650/Designing_a_Jump_in_Unity.php
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        distToGround = GetComponent<Collider2D>().bounds.extents.y;
+    }
+
+    void FixedUpdate(){
+         if(rb.velocity.y < 0) {
+            rb.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
+        }
+    }
+
+    void Update()
+    {
+        if(isGrounded()) {
+            Debug.Log("grounded");
+            if(jumpCount > 0){
+                jumpCount = 0;
+                Debug.Log("jump reset");    
+            }
+        }
+
+        if (Input.GetButtonDown("Jump")) {
+            if (canJump) {
+                if(jumpCount < maxJumpCount) {
+                    StartCoroutine(JumpRoutine());
+                    ++jumpCount;
+                }
+            }
+        }
+
+        
+
+       
+    }
+
+    IEnumerator JumpRoutine() {
+        // rb.velocity = Vector2.zero;
+
+        float timer = 0f;
+        Vector2 jumpVector = Vector2.up * strength;
+
+        while(Input.GetButton("Jump") && timer < jumpTime) {
+            float prop = timer/ jumpTime;
+            Vector2 force = Vector2.Lerp(jumpVector, Vector2.zero, prop);
+            rb.AddForce(force, ForceMode2D.Impulse);
+            timer += Time.deltaTime;
+            yield return null;       
+        }
+        
+    }
+
+    bool isGrounded() {
+        Debug.DrawRay(transform.position, Vector2.down, Color.green);
+        return Physics2D.Raycast(transform.position, Vector2.down, distToGround + 0.2f).collider != null;
+    }
+}
--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -2,24 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class movement : MonoBehaviour
+public class Move : MonoBehaviour
 {
     public float speed = 5;
-    public float strength = 15;
-    Rigidbody2D rb;
 
-    void Start(){
-
-        rb = GetComponent<Rigidbody2D>();
-    }
     void FixedUpdate(){
         float move = Input.GetAxis("Horizontal") * speed;
-        float jump = Input.GetAxis("Jump") * strength;
         
         move *= Time.deltaTime;
         
-
         transform.Translate(move, 0, 0);
-        rb.AddForce(Vector2.up * jump, ForceMode2D.Impulse);
     }
 }
