db38029c8844e111346baeeb4483cdfa26da11d2
Andre <ajq0@protonmail.com>
2020-01-26 15:57:43 +0100

    reorganized

---
+++ Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    public float speed = 5;
+    public float strength = 15;
+    Rigidbody2D rb;
+
+    void Start(){
+
+        rb = GetComponent<Rigidbody2D>();
+    }
+    void FixedUpdate(){
+        float move = Input.GetAxis("Horizontal") * speed;
+        float jump = Input.GetAxis("Jump") * strength;
+        
+        move *= Time.deltaTime;
+        
+
+        transform.Translate(move, 0, 0);
+        rb.AddForce(Vector2.up * jump, ForceMode2D.Impulse);
+    }
+}
